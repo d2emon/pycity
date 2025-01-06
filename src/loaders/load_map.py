78001b864c80aa562b5b22b816f24fcb5a0b3e4f
Dmitry Kutsenko <d2emonium@gmail.com>
2025-01-06 15:47:24 +0300

    Update map loader

--- src/loaders/load_map.py
+++ src/loaders/load_map.py
@@ -1,9 +1,30 @@
 def load_map(filename):
-        with open(filename, 'r') as file:
-            for line in file:
-                row = []
+    with open(filename, 'r') as file:
+        for line in file:
+            row = []
 
-                for tile_number in line.strip():
-                    row.append(int(tile_number))
+            for tile_number in line.strip():
+                tile_code = int(tile_number)
+                row.append(tile_code)
 
-                yield row
+            yield row
+
+
+def load_map_block(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            row = []
+            char_id = 0
+            tile_code = ''
+
+            for tile_number in line.strip():
+                tile_code += tile_number
+
+                if char_id >= 1:
+                    row.append(tile_code)
+                    char_id = 0
+                    tile_code = ''
+                else:
+                    char_id += 1
+
+            yield row
