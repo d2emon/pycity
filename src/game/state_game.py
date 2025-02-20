4f4acdf4782b34cf4d6198492ef2f23fc3ed6efc
Dmitry Kutsenko <d2emonium@gmail.com>
2025-02-20 16:17:13 +0300

    Cleanup state game

--- src/game/state_game.py
+++ src/game/state_game.py
@@ -14,7 +14,7 @@ class StateGame(Game):
     STATE_PLAYING = states.PLAYING
     STATE_WIN = states.WIN
 
-    screens = {}
+    state_screens = {}
 
     def __init__(
         self,
@@ -48,31 +48,12 @@ class StateGame(Game):
         game_logger.debug("Initializing game with states")
 
         # Set game fields
-        # self.players = []
-        # self.current_player_id = None  # Индекс текущего игрока
-        # self.field = None
-
         # self.game_is_over = False
         self.__state = self.STATE_INITIALIZATION
         # self.objects = []
 
-        # # Спрайты
-        # self.main_gui = MainGUI()
-        # self.player_panel = None
-        # self.main_panel = None
-        # self.tile_panel = None
-
-        self.__screen_group = pygame.sprite.Group()
-
-        # # Создание кнопки
-        # self.next_turn_button = None
-        # self.buy_property_button = None
-
-        # Set INIT error on init
-        # Set UPDATE error on update
-        # Set DRAW error on draw
-        # Set KEY_UP error on key up
-        # Set KEY_DOWN error on key down
+        # Create sprites
+        self.__group = pygame.sprite.Group()
 
     def load(self):
         # # Load resources
@@ -80,71 +61,25 @@ class StateGame(Game):
 
         super().load()
 
-        # # Игровые параметры
-        # self.players = [
-        #     Player(
-        #         0,
-        #         "Игрок 1",
-        #         (255, 0, 0),
-        #         # 0,
-        #         # 1000,
-        #         token=pygame.transform.scale(GameResources.get('portraits')[0], (64, 64)),
-        #         avatar=GameResources.get('portraits')[0],
-        #     ),
-        #     Player(
-        #         1,
-        #         "Игрок 2",
-        #         (0, 255, 0),
-        #         # 0,
-        #         # 1000,
-        #         token=pygame.transform.scale(GameResources.get('portraits')[1], (64, 64)),
-        #         avatar=GameResources.get('portraits')[1],
-        #     ),
-        # ]
-        # self.current_player_id = None  # Индекс текущего игрока
-
-        # # Создаем список клеток для игрового поля
-        # self.field = Field(GameResources.get('logos'))
-
-        # self.player_panel = self.main_gui.player_panel
-        # self.tile_panel = self.main_gui.tile_panel
-
-        # self.main_panel = MainPanel(
-        #     self.main_gui,
-        #     field=self.field,
-        # )
-        # self.main_gui.main_panel = self.main_panel
-
-        # # Создание кнопки
-        # self.next_turn_button = self.main_panel.turn_panel_group.next_turn_button
-        # self.next_turn_button.on_click = self.on_next_turn_button_click
-
-        # self.buy_property_button = self.main_panel.action_panel_group.buy_button
-        # self.buy_property_button.on_click = self.offer_property
-
-        # # Start player
-        # self.current_player_id = 0
-        # self.current_player.start_turn()
-
     # Getters and setters
 
     @property
-    def screen(self):
+    def state_screen(self):
         """Get current screen.
 
         Returns:
             pygame.sprite.Sprite: Current screen sprite.
         """
-        return self.__screen_group
+        return self.__group
 
-    @screen.setter
-    def screen(self, value):
+    @state_screen.setter
+    def state_screen(self, value):
         """Set current screen.
 
         Args:
             value (pygame.sprite.Sprite): New screen sprite.
         """
-        self.__screen_group = value
+        self.__group = value
 
         # Add screen events to handlers
         # Add custom events to handlers
@@ -168,13 +103,16 @@ class StateGame(Game):
         """
         game_logger.debug(f"[State: {value}]")
 
+        if self.__state == value:
+            return
+
         self.__state = value
 
-        screen = self.screens.get(value)
+        screen = self.state_screens.get(value)
         if screen is None:
             return
 
-        self.screen = screen(self)
+        self.state_screen = screen(self)
 
     @property
     def is_running(self):
@@ -228,41 +166,9 @@ class StateGame(Game):
     def update(self):
         super().update()
 
-        # # if pygame.key.get_pressed()[pygame.K_SPACE]:
-        # #     self.handle_turn(self.current_player_id)
-
-        # if self.current_player is not None:
-        #     self.main_panel.update_data(self.current_player, self.players)
-        #     self.player_panel.render(self.current_player)
-        #     self.tile_panel.render(self.current_player.turn, self.players)
-
-        #     for e in GameEvent.load(self.current_player.last_event_id):
-        #         self.process_event(e)
-
-        # self.main_gui.rect = self.screen.get_rect().inflate(0, -18)
-        # self.main_gui.update()
-
-        self.__screen_group.update()
+        self.__group.update()
 
     def draw(self):
-        # # Отрисовка игрового поля
-        # self.main_gui.draw(self.screen)
-        # self.main_panel.draw_panels(self.screen)
-
-        self.__screen_group.draw(self.window)
+        self.__group.draw(self.window)
 
         super().draw()
-
-    # Events
-
-    def on_game_event(self, event):
-        super().on_game_event(event)
-
-        # # Проверка нажатия кнопки мышью
-        # self.next_turn_button.process_event(event)
-
-        # # Проверка нажатия клавиши (например, пробел для следующего хода)
-        # if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        #     self.on_next_turn_button_click()
-
-        # self.main_panel.process_event(event)
