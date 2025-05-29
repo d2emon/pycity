0a1614e7b0a3d0df91e98c3172eb95c9cdd65521
Dmitry Kutsenko <d2emonium@gmail.com>
2025-05-29 15:36:59 +0300

    Refactor configuration settings by introducing window and color schema classes for improved organization and flexibility

--- src/config.py
+++ src/config.py
@@ -5,15 +5,50 @@ import os
 logging.basicConfig(level=logging.DEBUG)
 
 
-WIN_WIDTH = 640 # 800 # 640
-WIN_HEIGHT = 480 # 640 # 460 # 600
+class WindowConfig:
+    caption = "Mapper"
+    block_size = 8
+    width = block_size * 80
+    height = block_size * 60
 
-CAPTION = "Mapper"
+
+class BigWindowConfig(WindowConfig):
+    width = 800
+    height = 640
+
+
+class SmallWindowConfig(WindowConfig):
+    width = 640
+    height = 480
+
+
+class ColorSchema:
+    background = 0, 0, 0
+
+
+class ColorSchema1:
+    background = 30, 150, 50
+
+
+class ColorSchema2:
+    background = 0, 153, 255
+
+
+window_config = SmallWindowConfig
+color_schema = ColorSchema2
+
+CAPTION = window_config.caption
+WIN_WIDTH = window_config.width
+WIN_HEIGHT = window_config.height
+
 WINDOW_SIZE = WIN_WIDTH, WIN_HEIGHT
-BACKGROUND_COLOR = 0, 0, 0 # 30, 150, 50 # "#0099FF"
+
+BACKGROUND_COLOR = color_schema.background
+
 DELAY = 16
 # FPS = 60
 
+## Player
 
 # PLAYER_POS = (WIN_WIDTH / 2, WIN_HEIGHT / 2)
 # MAP_POS = (0, 0)
@@ -25,21 +60,9 @@ BASE_PATH = os.path.abspath(os.path.join(__file__, '..', '..', 'res'))
 
 ## Config
 
-"""
-# > games.map_walk
-# > games.walker
-# > space
-class SCREEN:
-    WIDTH = WIN_WIDTH
-    HEIGHT = WIN_HEIGHT
-    SIZE = WIDTH, HEIGHT
-    CAPTION = CAPTION
-    CENTER = (WIDTH / 2, HEIGHT / 2)
+## > space
 
-PLAYER_POS = WIN_WIDTH / 2, WIN_HEIGHT / 2
-MAP_POS = (0, 0)
-BACKGROUND_COLOR = "#0099FF"
-# > space
+"""
 FRAME_RATE = 60
 GRID_SIZE = 32
 
@@ -47,11 +70,12 @@ PLAYER_SIZE = 10
 PLAYER_VIEW = 5 * SCALE
 PLAYER_COLOR = (255, 255, 0)
 PLAYER_VIEW_COLOR = (0, 0, 255)
+"""
 
-# > games.walker.resource
+## > games.walker.resource
+
+"""
 class Universe:
-    ### ???
-    # BASE_PATH = os.path.join(files.BASE_PATH, 'walker', 'universe')
     GLOBAL_MAP = os.path.join(BASE_PATH, 'universe.png')
     PLAYER = os.path.join(BASE_PATH, 'saucer.png')
 """
