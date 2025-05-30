b556b30486c465990af78f4fc61a07ccf007997b
Dmitry Kutsenko <d2emonium@gmail.com>
2025-05-30 13:46:33 +0300

    Add methods for background, player, and level creation in ScreenGroup class to enhance game initialization structure.

--- src/sprites/screen.py
+++ src/sprites/screen.py
@@ -7,3 +7,16 @@ class ScreenGroup(pygame.sprite.LayeredUpdates):
 
         self.window = window
         self.rect = window.get_rect()
+
+        self.background = self.create_background()
+        self.player = self.create_player()
+        self.level = self.create_level()
+
+    def create_background(self):
+        return None
+
+    def create_player(self):
+        return None
+
+    def create_level(self):
+        return None
