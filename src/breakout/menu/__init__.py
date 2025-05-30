a8a0bbb9fb50fd1aad70c91a90fee2db41ff2359
Dmitry Kutsenko <d2emonium@gmail.com>
2025-05-30 13:58:08 +0300

    Refactor MenuScreenGroup class to inherit from MenuScreenGroup, streamline menu item creation, and enhance background management for improved organization and initialization.

--- src/breakout/menu/__init__.py
+++ src/breakout/menu/__init__.py
@@ -1,28 +1,16 @@
 import pygame
-from game import events
 from sprites.image import Image
-from sprites.menu_items import MenuItems
-from sprites.screen import ScreenGroup
+from sprites.screen import MenuScreenGroup
 from .item import MainMenuItem
 from .resources import MenuResources
 
 
-class MenuScreenGroup(ScreenGroup):
-    background_image = "res/global/map.jpg"
-
-    def __init__(self, window, *spites):
-        super().__init__(window, *spites)
-
-        self.menu_items = MenuItems(
-            MainMenuItem("PLAY", event_type="BUTTON_PLAY"),
-            MainMenuItem("QUIT", event_type="BUTTON_QUIT"),
-        )
-        for menu_item in self.menu_items:
-            self.add(menu_item, layer=10)
-
-    # ScreenGroup loaders
-
+class MenuScreenGroup(MenuScreenGroup):
     def create_background(self):
         background = Image(self.rect, MenuResources.get('background'))
         self.add(background)
         return background
+
+    def create_menu_items(self):
+        yield MainMenuItem("PLAY", event_type="BUTTON_PLAY")
+        yield MainMenuItem("QUIT", event_type="BUTTON_QUIT"),
--- src/menu_screen/menu/__init__.py
+++ src/menu_screen/menu/__init__.py
@@ -1,28 +1,18 @@
 import pygame
-from game.state_game import StateGame
 from sprites.image import Image
-from sprites.menu_items import MenuItems
-from sprites.screen import ScreenGroup
+from sprites.screen import MenuScreenGroup
 from .item import MainMenuItem
 from .resources import MenuResources
 
 
-class MenuScreenGroup(ScreenGroup):
-    def __init__(self, window, *spites):
-        super().__init__(window, *spites)
-
-        self.menu_items = MenuItems(
-            MainMenuItem("Play", event_type="BREAKOUT"),
-            MainMenuItem("Walk Map", event_type="MAP_WALK"),
-            MainMenuItem("City", event_type="CITY"),
-            MainMenuItem("Quit", event_type="QUIT"),
-        )
-        for menu_item in self.menu_items:
-            self.add(menu_item, layer=10)
-
-    # ScreenGroup loaders
-
+class MenuScreenGroup(MenuScreenGroup):
     def create_background(self):
         background = Image(self.rect, MenuResources.get('background'))
         self.add(background)
         return background
+
+    def create_menu_items(self):
+        yield MainMenuItem("Play", event_type="BREAKOUT"),
+        yield MainMenuItem("Walk Map", event_type="MAP_WALK"),
+        yield MainMenuItem("City", event_type="CITY"),
+        yield MainMenuItem("Quit", event_type="QUIT"),
