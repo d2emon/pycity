bb3ab81644b50ccea2a8ee45893b0e5ad979acb8
Dmitry Kutsenko <d2emonium@gmail.com>
2025-06-04 16:59:31 +0300

    Add Tiles class for world map tile management
    - Introduced a new Tiles class to manage tile placement and validation within the world map.
    - Implemented methods for getting and setting tiles, validating positions, and loading tiles from a heightmap.
    - Enhanced modularity and clarity in world generation by encapsulating tile logic in a dedicated class.

---
+++ src/procgen/world_map/tiles.py
@@ -0,0 +1,86 @@
+from procgen.main.sprites import tiles
+from procgen.main.sprites.map_points import MapPoint
+from procgen.main.worldgen.roads import Road, Roads
+
+
+class Tiles:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__tiles = [
+            [None for _ in range(width)]
+            for _ in range(height)
+        ]
+
+    # Helpers
+
+    @classmethod
+    def int_pos(cls, pos):
+        return int(pos[0]), int(pos[1])
+
+    # Validators
+
+    def is_valid_pos(self, pos):
+        x, y = pos
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    # Getters
+
+    def get_valid_pos(self, pos):
+        x, y = self.int_pos(pos)
+
+        if not self.is_valid_pos((x, y)):
+            return None
+
+        return x, y
+
+    # Tile methods
+
+    def get_tile(self, pos):
+        valid_pos = self.get_valid_pos(pos)
+
+        if valid_pos is None:
+            return None
+
+        x, y = valid_pos
+        return self.__tiles[y][x]
+
+    def set_tile(self, pos, value):
+        valid_pos = self.get_valid_pos(pos)
+
+        if valid_pos is None:
+            return
+
+        x, y = valid_pos
+        self.__tiles[y][x] = value
+
+    ####
+
+    @classmethod
+    def load(cls, heightmap, tile_map):
+        tiles = cls(
+            heightmap.width,
+            heightmap.height,
+        )
+
+        for pos, value in heightmap.values:
+            tile = tiles.tile_by_value(value)
+            tile.rect = tile_map.get_tile(pos)
+            tiles.set_tile(pos, tile)
+
+        return tiles
+
+    @classmethod
+    def tile_by_value(cls, value):
+        water_level = -0.2
+        grass_level = 0
+        rock_level = 0.2
+
+        if value < water_level:
+            return tiles.Water(value)
+        elif value < grass_level:
+            return tiles.Sand(value)
+        elif value < rock_level:
+            return tiles.Grass(value)
+        else:
+            return tiles.Rock(value)
