a69273b1d2991928288bdd5b3b378f42185047cb
Dmitry Kutsenko <d2emonium@gmail.com>
2025-06-04 13:05:10 +0300

    Refactor World class to utilize world_map's tile_map for improved clarity
    - Removed direct TileMap instantiation in the World class, now referencing world_map's tile_map for tile retrieval.
    - Updated MapPoint initialization to use world_map's tile size, enhancing consistency in world generation logic.

--- src/procgen/main/worldgen/world.py
+++ src/procgen/main/worldgen/world.py
@@ -1,13 +1,9 @@
-import config
 from ..sprites import tiles
 from ..sprites.map_points import MapPoint
 from .roads import Road, Roads
-from .tile_map import TileMap
 
 
 class World:
-    tile_map = TileMap(config.TILE_SIZE)
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
@@ -43,14 +39,14 @@ class World:
 
         for pos, value in world_map.heightmap.values:
             tile = self.tile_by_value(value)
-            tile.rect = self.tile_map.get_tile(pos)
+            tile.rect = world_map.tile_map.get_tile(pos)
             self.set_tile(pos, tile)
 
         self.points = []
         for map_object in world_map.map_points:
             pos = map_object.pos
-            point = MapPoint(pos, self.tile_map.tile_size)
-            point.rect = self.tile_map.get_tile(pos)
+            point = MapPoint(pos, world_map.tile_map.tile_size)
+            point.rect = world_map.tile_map.get_tile(pos)
             self.points.append(point)
 
         self.roads = Roads()
