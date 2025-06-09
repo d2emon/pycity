e3430cc32437650c770a5bd9c278f1438d2a4dce
Dmitry Kutsenko <d2emonium@gmail.com>
2025-06-09 18:21:08 +0300

    Add generate_equally method to PointFactory for structured point generation
    - Introduced the `generate_equally` method in the `PointFactory` class to yield points at regular intervals, enhancing control over point distribution in procedural generation.
    - This addition complements the existing random point generation functionality, providing an alternative method for generating points in a grid-like pattern.

--- src/procgen/main/worldgen/point_factory.py
+++ src/procgen/main/worldgen/point_factory.py
@@ -13,3 +13,8 @@ class PointFactory:
             x = random.randrange(0, self.width)
             y = random.randrange(0, self.height)
             yield x, y
+
+    def generate_equally(self):
+        for x in range(0, self.width, 12):
+            for y in range(0, self.height, 12):
+                yield x, y
