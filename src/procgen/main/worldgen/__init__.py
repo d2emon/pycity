a641075d1749aed8c1b1d1dfaf4eb90710137f79
Dmitry Kutsenko <d2emonium@gmail.com>
2025-06-11 19:57:15 +0300

    Add logging and universe generation in worldgen module
    - Introduced logging functionality to track model details during world generation.
    - Integrated `UniverseFactory` and `SizeFactory` to create and manage universe dimensions, enhancing the procedural generation of nested structures.
    - Updated the `generate_world` function to instantiate and log the universe and its superclusters, improving visibility into the generation process.

--- src/procgen/main/worldgen/__init__.py
+++ src/procgen/main/worldgen/__init__.py
@@ -1,9 +1,19 @@
+import logging
 from procgen.world_map.world import World
 from .point_factory import PointFactory
 from .road_factory import RoadFactory
 from .tile_factory import TileFactory
 from .tile_road_factory import generate_roads
 from .voronoi_factory import VoronoiFactory
+
+from .nested.universe import UniverseFactory
+from .nested.factory import SizeFactory
+
+
+def log_model(model):
+    logging.debug("Model: %s (%s)", model, model.factory)
+    logging.debug("Size: (%s, %s) in %s", model.width, model.height, model.pos)
+    logging.debug("Name: %s", model.name)
 
 
 def generate_world(width, height, tile_size):
@@ -15,6 +25,17 @@ def generate_world(width, height, tile_size):
         map_name="Voronoi Map",
         generator="VoronoiMapGenerator",
     )
+
+    universe_size_factory = SizeFactory()
+    universe_size_factory.min_width = universe_size_factory.max_width = width
+    universe_size_factory.min_height = universe_size_factory.max_height = height
+
+    universe_factory = UniverseFactory()
+    universe_factory.size_factory = universe_size_factory
+    universe = universe_factory()
+    log_model(universe)
+    for supercluster in universe.children:
+        log_model(supercluster)
 
     tile_factory = TileFactory()
     world.heightmap = tile_factory.generate(width, height)
